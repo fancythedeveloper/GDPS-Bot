27affeb967eb9c2ae77a5ba24b3c6c8db77b4602
fancymelonhub <30706150+fancythedeveloper@users.noreply.github.com>
2021-05-17 09:59:45 -0400

    Fixing grammar maybe

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ const botconfig = require("../botsettings.json");
 module.exports.run = async (bot, message, args) => {
     const help = new Discord.MessageEmbed()
         .setTitle('__Help and Commands__')
-        .addField('__Help__', 'if you need support please contact [wylie](https://twitter.com/TheWylieMaster). he can be contacted [here](https://discord.gg/r5KW2bR)')
+        .addField('__Help__', 'If you need support, try to contact [Wylie](https://twitter.com/TheWylieMaster). He can also be contacted [here](https://discord.gg/r5KW2bR)').
         .addField('__Commands__', '`' + botconfig.prefix + 'profile {Player name or Account ID}`\n\n`' + botconfig.prefix + 'level {Level name or Level ID}`\n\n`' + botconfig.prefix + 'leaderboard {star/cp}`')
         .setColor('0x' + botconfig.commandColour)
     message.channel.send(help)
